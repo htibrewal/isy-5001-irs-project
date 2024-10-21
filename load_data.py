3727cf31a2c1f45e3a63ee02b5dde7b51577ceec
harsh <harshtibrewal18@gmail.com>
2024-10-21 20:01:05 +0800

    feat: add logic to build line items array for purchase order and check if VendorItem is already present in the list

--- load_data.py
+++ load_data.py
@@ -3,26 +3,28 @@ import pandas as pd
 from dotenv import load_dotenv
 
 from blocks.ElectricalPart import ElectricalPart
+from blocks.LineItem import LineItem
 from blocks.Vendor import Vendor
 from blocks.VendorItem import VendorItem
 
 load_dotenv()
 
 def fetch_electrical_parts() -> dict[str, ElectricalPart]:
-    electrical_parts_data = pd.read_csv(os.path.join(os.getenv('ELECTRICAL_PARTS_FOLDER_PATH', 'Electrical Parts Report Modified.csv')))
+    electrical_parts_data = pd.read_csv(os.path.join(os.getenv('PROCESSED_DATA_FOLDER_PATH'), 'electrical_parts_final.csv'))
 
     electrical_parts = dict()
     for _, electrical_part in electrical_parts_data.iterrows():
         part_id = electrical_part['PART_ID_CLEANED']
 
-        electrical_parts[part_id] = ElectricalPart(
+        electrical_parts[str(part_id)] = ElectricalPart(
             part_id,
             electrical_part['PART_NAME'],
             description=electrical_part['PART_DESCRIPTION'],
             width=electrical_part['WIDTH_(MM)'],
             height=electrical_part['HEIGHT_(MM)'],
             depth=electrical_part['DEPTH_(MM)'],
-            weight=electrical_part['WEIGHT_(KG)'],)
+            weight=electrical_part['WEIGHT_(KG)']
+        )
 
     return electrical_parts
 
@@ -51,7 +53,7 @@ def fetch_vendor_items(purchase_order_data: pd.DataFrame, electrical_parts: dict
         item_code = line_item['ITEM_CODE_CLEANED']
         vendor_code = line_item['SUPPLIER_CODE']
 
-        electrical_part = electrical_parts[item_code]
+        electrical_part = electrical_parts[str(item_code)]
         vendor = vendors[vendor_code]
 
         if electrical_part not in items_vendor_map:
@@ -60,6 +62,16 @@ def fetch_vendor_items(purchase_order_data: pd.DataFrame, electrical_parts: dict
         tax_percent = line_item['TAX_AMOUNT(LC)']/line_item['ITEM_VALUE'] * 100
         vendor_item = VendorItem(electrical_part, vendor, line_item['PRICE'], tax_percent)
 
-        items_vendor_map[electrical_part].append(vendor_item)
+        if vendor_item not in items_vendor_map[electrical_part]:
+            items_vendor_map[electrical_part].append(vendor_item)
 
     return items_vendor_map
+
+def build_line_items(raw_data: dict[str, int], electrical_parts: dict[str, ElectricalPart]) -> list[LineItem]:
+    line_items = list()
+
+    for item_code, quantity in raw_data.items():
+        electrical_part = electrical_parts[item_code]
+        line_items.append(LineItem(electrical_part, quantity))
+
+    return line_items
