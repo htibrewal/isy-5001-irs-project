62c1b231ab769189052ea6ef745635056faf8353
harsh <harshtibrewal18@gmail.com>
2024-10-23 23:09:13 +0800

    feat: change entire flow of main file to use objects of GA

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ from blocks.PurchaseOrder import PurchaseOrder
 from blocks.Vendor import Vendor
 from load_data import load_purchase_order_data, fetch_vendors, fetch_electrical_parts, fetch_vendor_items, \
     build_line_items
-from muliple_vendors import multi_genetic_algorithm, multi_fitness_score
-from single_vendor import single_genetic_algorithm, single_fitness_score
+from multi_vendor_ga import MultiVendorGA
+from single_vendor_ga import SingleVendorGA
+from optimisation_type import OptimisationType
 
 load_dotenv()
 
@@ -22,25 +23,54 @@ def main():
 
     items_vendor_map = fetch_vendor_items(purchase_order_data, electrical_parts, vendors)
 
+    # create objects for SingleVendorGA and MultiVendorGA
+    single_vendor_ga = SingleVendorGA(items_vendor_map, list(vendors.values()))
+    multiple_vendor_ga = MultiVendorGA(items_vendor_map)
+
+    # create PurchaseOrder object
     buyer_order = {'17833': 4, '17437': 10, '18087': 2, '15102': 15}
     purchase_order = PurchaseOrder(build_line_items(buyer_order, electrical_parts))
 
-    # Trying multiple vendors solution
-    multi_vendors_solution = multi_genetic_algorithm(purchase_order, items_vendor_map)
 
-    for electrical_part, vendor_item in multi_vendors_solution.items():
+    ## Cost
+    print("\n------------Cost Optimisation-----------")
+
+    print("\nSingle Vendor")
+    single_vendor = single_vendor_ga.solve(purchase_order)
+    print(single_vendor)
+
+    cost = -1 * single_vendor_ga.compute_fitness_score(purchase_order, single_vendor)
+    print(f"Cost = {cost: .2f}")
+
+
+    print("\nMultiple Vendor")
+    multiple_vendors = multiple_vendor_ga.solve(purchase_order)
+    for electrical_part, vendor_item in multiple_vendors.items():
         print(f"{electrical_part} -> {vendor_item.vendor}")
 
-    cost = -1 * multi_fitness_score(purchase_order, multi_vendors_solution)
+    cost = -1 * multiple_vendor_ga.compute_fitness_score(purchase_order, multiple_vendors)
     print(f"Cost = {cost: .2f}")
 
 
-    # Trying single vendor solution
-    single_vendor_solution = single_genetic_algorithm(purchase_order, items_vendor_map, list(vendors.values()))
-    print(single_vendor_solution)
+    ## Delivery Days
+    print("\n-----------Delivery Optimisation-----------")
+    optimisation = OptimisationType.DELIVERY
 
-    cost = -1 * single_fitness_score(purchase_order, single_vendor_solution, items_vendor_map)
-    print(f"Cost = {cost: .2f}")
+    print("\nSingle Vendor")
+    single_vendor = single_vendor_ga.solve(purchase_order, optimisation=optimisation)
+    print(single_vendor)
+
+    days = -1 * single_vendor_ga.compute_fitness_score(purchase_order, single_vendor, optimisation=optimisation)
+    print(f"Delivery in Days = {days}")
+
+
+    print("\nMultiple Vendor")
+    multiple_vendors = multiple_vendor_ga.solve(purchase_order, optimisation=optimisation)
+    for electrical_part, vendor_item in multiple_vendors.items():
+        print(f"{electrical_part} -> {vendor_item.vendor}")
+
+    days = -1 * multiple_vendor_ga.compute_fitness_score(purchase_order, multiple_vendors, optimisation=optimisation)
+    print(f"Delivery in Days = {days}")
 
 
 if __name__ == '__main__':
